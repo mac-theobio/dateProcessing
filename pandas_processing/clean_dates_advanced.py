35e1d4c3b810a2389f6ae28e9c2d242680422d53
Apeirogons <43548362+Apeirogons@users.noreply.github.com>
2020-08-01 04:08:17 -0400

    Update clean_dates_advanced.py
    Processed example2, coerced example2 to 2020, fixed bug where invalid dates would only be removed if ambiguous, added condition where 'nearby' dates must be within 1 year of either of the two possible dates if ambiguous

--- pandas_processing/clean_dates_advanced.py
+++ pandas_processing/clean_dates_advanced.py
@@ -11,16 +11,6 @@ from copy import deepcopy
 
 
 # %%
-# Load sheet
-sheet_0 = pd.read_excel('example1_adj.xlsx')
-
-
-# %%
-initial_valid_date = pd.to_datetime('2020-Feb-01') # Initial time specified in Jonathan's email
-final_valid_date = pd.to_datetime(getmtime('example1_adj.xlsx'), unit='s') # Time of last modification
-
-
-# %%
 def get_unambiguous_date_values(col, initial_valid_date, final_valid_date):
     """
     Given an input date column, return a column cleaned of any invalid dates, while enforcing that each date must be between 
@@ -38,10 +28,18 @@ def get_unambiguous_date_values(col, initial_valid_date, final_valid_date):
         converted_first_datetime = pd.to_datetime(date, errors='coerce').date() # Attempt to coerce each cell to datetime format.
 
         if not pd.isna(converted_first_datetime):  # If time cannot be coerced to datetime, then it cannot be unambiguous.
+            is_original_valid = (converted_first_datetime >= initial_valid_date) and (converted_first_datetime <= final_valid_date)
+
             if converted_first_datetime.day > 12: # If the day > 12, then datetime is not ambiguous as there is no month with number > 12.
-                this_column.append(converted_first_datetime)
+                if is_original_valid:
+                    this_column.append(converted_first_datetime)
+                else:
+                    this_column.append('%COMPLETELY_INVALID_DATE%')
             elif converted_first_datetime.day == converted_first_datetime.month: # If day == month, we don't care what the exact formatting was.
-                this_column.append(converted_first_datetime)
+                if is_original_valid:
+                    this_column.append(converted_first_datetime)
+                else:
+                    this_column.append('%COMPLETELY_INVALID_DATE%')
             else: # We then test for dates being within initial_valid_date and final_valid_date. 
                 str_original = str(converted_first_datetime)
                 alternative_date = pd.to_datetime(str_original[:4] + '-' + str_original[-2:] + '-' + str_original[5:7]).date()
@@ -56,7 +54,7 @@ def get_unambiguous_date_values(col, initial_valid_date, final_valid_date):
                 elif is_original_valid and is_alternative_valid: # If both are valid, then datetime is ambiguous.
                     this_column.append('%AMBIGUOUS_VALID_DATE%')
                 elif (not is_original_valid) and (not is_alternative_valid): # If neither are valid, then there must have been an input error.
-                    this_column.append('%COMPLETELY_INVALID_DATE')
+                    this_column.append('%COMPLETELY_INVALID_DATE%')
                 else:
                     raise Exception('This line should never run. This is left for debugging purposes.')
         else:
@@ -80,9 +78,14 @@ def line_fill_ambiguous_date(ambiguous_column, original_column, searchradius = 5
 
     for i, date in enumerate(ambiguous_column):
         if date == '%AMBIGUOUS_VALID_DATE%':
+            original_date = pd.to_datetime(original_column[i], errors='coerce').date() 
+            str_original = str(original_date)
+            alternative_date = pd.to_datetime(str_original[:4] + '-' + str_original[-2:] + '-' + str_original[5:7]).date()
+            
             dates_nearby = np.concatenate([ambiguous_column[max(i-searchradius, 0):i], ambiguous_column[i+1: min(i+1+searchradius, len(ambiguous_column))]]) # Look for dates vertically in the file with a search radius of searchradius
             dates_nearby = sorted(list(filter(lambda x: type(x)==datetime.date, dates_nearby))) # Filter those dates which are actually dates, and non-ambiguous
-
+            dates_nearby = list(filter(lambda x: (np.abs(x-original_date).days < 365) or (np.abs(x-alternative_date).days < 365), dates_nearby)) # Filter those dates which are within a year of the possible date of comparison
+            
             # If there exist dates nearby, take the median of these dates.
             if dates_nearby != []: 
                 median_date = dates_nearby[int(len(dates_nearby)/2)]
@@ -149,35 +152,54 @@ def row_fill_ambiguous_date(ambiguous_df, original_df):
     return new_df
 
 
-# %%
-unambiguous_sheet = pd.DataFrame()
-for col in sheet_0.columns:
-    if col.find('date') != -1:
-        unambiguous_sheet[col] =get_unambiguous_date_values(sheet_0[col], initial_valid_date, final_valid_date)
-    else: 
-        unambiguous_sheet[col] = sheet_0[col]
+for sheet_name in ['example1_adj.xlsx', 'example2_adj.xlsx']:
+    # Load sheet
+    sheet_0 = pd.read_excel(sheet_name) #'example1_adj.xlsx')
 
-for col in sheet_0.columns:
-    i = 0
-    if col.find('date') != -1:
-        last_col_version = deepcopy(unambiguous_sheet[col])
-        unambiguous_sheet[col] = line_fill_ambiguous_date(unambiguous_sheet[col], sheet_0[col]) 
-        while not unambiguous_sheet[col].equals(last_col_version):
+    initial_valid_date = pd.to_datetime('2020-Feb-01') # Initial time specified in Jonathan's email
+
+    final_valid_date = pd.to_datetime(getmtime(sheet_name), unit='s') # Time of last modification
+
+    # coerce all dates to 2020
+    for col in sheet_0.columns:
+        replacement_row = []
+        if col.find('date') != -1:
+            for date in sheet_0[col]:
+                converted_first_datetime = pd.to_datetime(date, errors='coerce').date() # Attempt to coerce each cell to datetime format.
+                try:
+                    if not pd.isna(converted_first_datetime):
+                        converted_first_datetime = pd.to_datetime("2020" + str(converted_first_datetime)[4:], errors='coerce').date()
+                    else: 
+                        converted_first_datetime = pd.NaT
+                except IndexError:
+                    converted_first_datetime = pd.NaT
+                replacement_row.append(converted_first_datetime)
+            sheet_0[col]= replacement_row
+
+    unambiguous_sheet = pd.DataFrame()
+    for col in sheet_0.columns:
+        if col.find('date') != -1:
+            unambiguous_sheet[col] =get_unambiguous_date_values(sheet_0[col], initial_valid_date, final_valid_date)
+        else: 
+            unambiguous_sheet[col] = sheet_0[col]
+
+    for col in sheet_0.columns:
+        i = 0
+        if col.find('date') != -1:
             last_col_version = deepcopy(unambiguous_sheet[col])
-            unambiguous_sheet[col] = line_fill_ambiguous_date(unambiguous_sheet[col], sheet_0[col]) 
-            i += 1
-            if i > 100:
-                break
-        print(i)
+            unambiguous_sheet[col] = line_fill_ambiguous_date(unambiguous_sheet[col], sheet_0[col], 7) 
+            while not unambiguous_sheet[col].equals(last_col_version):
+                last_col_version = deepcopy(unambiguous_sheet[col])
+                unambiguous_sheet[col] = line_fill_ambiguous_date(unambiguous_sheet[col], sheet_0[col], 7) 
+                i += 1
+                if i > 100:
+                    break
+            print(i)
 
-unambiguous_sheet = row_fill_ambiguous_date(unambiguous_sheet, sheet_0)
+    unambiguous_sheet = row_fill_ambiguous_date(unambiguous_sheet, sheet_0)
+
+    unambiguous_sheet.to_excel(sheet_name.split('.')[0] +'_cleaned.xlsx', index=False)
+
 
 
 # %%
-unambiguous_sheet.to_excel('example1_cleaned_advanced.xlsx', index=False)
-
-
-# %%
-
-
-
